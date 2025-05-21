a9014ffe2e7b501da0e0154dd18e80d432c12e73
paamelaamaro <paamelaamaro.silva@gmail.com>
2025-05-21 20:34:04 -0300

    Ajustes no mobile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
+
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+}
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
@@ -19,4 +26,5 @@ exports.default = gulp.parallel(styles, images);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+     gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
